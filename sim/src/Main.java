827be0139731ab3e7bf798911e9d7b532733252a
Tu Nombre <you@example.com>
2021-12-19 19:44:43 +0100

    añadida funcion de generacion aleatoria con seed

--- sim/src/Main.java
+++ sim/src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         Event a = new Event("event1");
         a.printTimestamp("hola");
+        SimState s = new SimState();
+        Orquestrator o = new Orquestrator(0,100,s,0);
+        Event[] e = new Event[1];
+        o.generateActors(5,e);
     }
 }
--- sim/src/Orquestrator.java
+++ sim/src/Orquestrator.java
@@ -1,21 +1,38 @@
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
 
 public class Orquestrator {
     float currtime;
-    float maxtime;
+    int maxtime;
     SimState simState;
     List<Actor> actors;
+    private Random fRandom;
 
-    public Orquestrator(float currtime, float maxtime, SimState simState) {
+    public Orquestrator(float currtime, int maxtime, SimState simState,long seed) {
         this.currtime = currtime;
         this.maxtime = maxtime;
         this.simState = simState;
+        fRandom = new Random(seed);
     }
 
     public void setUp(){}
-    public void generateActors(){
+
+    public void generateActors(Integer numActors,Event[] events){
         //llenamos la lista de actores mediante una distribucion o algo y luego se ordenan por spawntime
+        List<Actor> Actors = new ArrayList<>();
+        for(int i = 0; i < numActors; ++i){
+            Float rn = fRandom.nextFloat(0,maxtime);
+            Actor a = new Actor("Actor" + rn.toString() ,events,simState,rn);
+            Actors.add(a);
+        }
+        System.out.println(Actors);
+        for(Actor a: Actors){
+            System.out.println(Float.toString(a.spawnTime) + " " + a.remainingEvents.toString() + " " + a.simState.toString() + " " + a.name);
+        }
     };
 
     public void runSim(){
@@ -55,4 +72,9 @@ public class Orquestrator {
         }
     };
 
+
+    private Random generator = new Random();
+    double randomGenerator() {
+        return generator.nextDouble()*0.5;
+    }
 }
